7cc9fb4d974d3f275bd7445a84fc85a1dccb3be3
zeta-squared <zeaiter.zeaiter@hotmail.com>
2025-05-03 11:28:50 +1000

    chore: added missing install for vim-plug

--- setup.sh
+++ setup.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-mkdir -p $HOME/.local/share/nvim
-mkdir -p $HOME/.config/nvim
-cp -r -t $HOME/.local/share/nvim nvim/UltiSnips nvim/spell
-cp -r -t $HOME/.config/nvim nvim/lua nvim/ftplugin nvim/init.lua
-cp nvim/pycodestyle $HOME/.config
-
 packages=(
     ripgrep
     fzf
@@ -21,5 +15,17 @@ done
 
 echo "All packages from the setup script have been installed."
 
+echo "Installing dotfiles..."
+mkdir -p $HOME/.local/share/nvim
+mkdir -p $HOME/.config/nvim
+cp -r -t $HOME/.local/share/nvim nvim/UltiSnips nvim/spell
+cp -r -t $HOME/.config/nvim nvim/lua nvim/ftplugin nvim/init.lua
+cp nvim/pycodestyle $HOME/.config
 cp -f .bashrc $HOME
 cp -rf -t $HOME/.config gh lazygit
+echo "Dotfiles have been installed."
+
+echo "Install vim-plug..."
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+echo "Vim-plug has been installed."
